8441f99885994cecbe52616e7f1a25da0bfe6d49
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-04-26 16:34:37 +0200

    modified project structure, added library 'classnames'

---
+++ src/containers/App/App.js
@@ -0,0 +1,8 @@
+import cn from 'classnames'
+import styles from './App.module.css'
+
+const App = () => {
+  return <h1 className={cn(styles.header, styles.text)}>Hello</h1>
+}
+
+export default App
