f94e72d2041eed1fcc14a4dfc1deb3a733658883
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-04-28 15:27:41 +0200

    created PeoplePage container

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,9 @@
+import PeoplePage from '../PeoplePage'
+
 import styles from './App.module.css'
 
-import { getApiResource } from '../../utils/network'
-
 const App = () => {
-  return <h1 className={styles.header}>Hello</h1>
+  return <PeoplePage />
 }
 
 export default App
