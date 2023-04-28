a3171d8c65e4cc25e53b68ec6901f17ba6dc3cfc
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-04-28 15:25:56 +0200

    created file 'api.js' with constants, created service file 'getPeopleData.js'

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,6 +1,3 @@
-const SWAPI_ROOT = 'https://swapi.dev/api/'
-const SWAPI_PEOPLE = 'people'
-
 export const getApiResource = async (url) => {
   try {
     const res = await fetch(url)
@@ -16,7 +13,3 @@ export const getApiResource = async (url) => {
     return false
   }
 }
-;(async () => {
-  const body = await getApiResource(SWAPI_ROOT + SWAPI_PEOPLE)
-  console.log(body)
-})()
