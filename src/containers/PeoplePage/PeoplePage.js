becb9bab75190cd3b5efc70e060ebcd82339974e
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-04-30 16:09:25 +0200

    created HOC withErrorApi, created aliases for paths

--- src/containers/PeoplePage/PeoplePage.js
+++ src/containers/PeoplePage/PeoplePage.js
@@ -1,35 +1,46 @@
 import { useState, useEffect } from 'react'
-import { getApiResource } from '../../utils/network'
-import { API_PEOPLE } from '../../constants/api'
-import { getPeopleId, getPeopleImage } from '../../services/getPeopleData'
-import PeopleList from '../../components/PeoplePage/PeopleList'
+import { withErrorApi } from '@hoc-helpers/withErrorApi'
+import { getApiResource } from '@utils/network'
+import { API_PEOPLE } from '@constants/api'
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData'
+import PeopleList from '@components/PeoplePage/PeopleList'
 
 import styles from './PeoplePage.module.css'
 
-const PeoplePage = () => {
+const PeoplePage = ({ setErrorApi }) => {
   const [people, setPeople] = useState(null)
 
   const getResource = async (url) => {
     const res = await getApiResource(url)
 
-    const peopleList = res.results.map(({ name, url }) => {
-      const id = getPeopleId(url)
-      const img = getPeopleImage(id)
-      return {
-        id,
-        name,
-        img,
-      }
-    })
+    if (res) {
+      const peopleList = res.results.map(({ name, url }) => {
+        const id = getPeopleId(url)
+        const img = getPeopleImage(id)
+        return {
+          id,
+          name,
+          img,
+        }
+      })
 
-    setPeople(peopleList)
+      setPeople(peopleList)
+      setErrorApi(false)
+    } else {
+      setErrorApi(true)
+    }
   }
 
   useEffect(() => {
     getResource(API_PEOPLE)
   }, [])
 
-  return <>{people && <PeopleList people={people} />}</>
+  return (
+    <>
+      <h1>Navigation</h1>
+      {people && <PeopleList people={people} />}
+    </>
+  )
 }
 
-export default PeoplePage
+export default withErrorApi(PeoplePage)
--- src/services/getPeopleData.js
+++ src/services/getPeopleData.js
@@ -4,7 +4,7 @@ import {
   SWAPI_PEOPLE,
   GUIDE_IMG_EXTENSION,
   URL_IMG_PERSON,
-} from '../constants/api'
+} from '@constants/api'
 
 const getId = (url, category) => {
   const id = url.replace(HTTP + SWAPI_ROOT + category, '').replace(/\//g, '')
