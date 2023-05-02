0f6d5d866c27a4eb5668cde89a072de43fb8349f
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-05-02 11:02:01 +0200

    added changeHTTP function, created ducumentation of the functions in 'network.js'

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,3 +1,22 @@
+import { HTTP, HTTPS } from '@constants/api'
+
+/**
+ * Changing URL from HTTP to HTTPS
+ * @param {String} url - url to be changed
+ * @returns {String} - url with HTTPS
+ */
+
+export const changeHTTP = (url) => {
+  const result = url ? url.replace(HTTP, HTTPS) : url
+  return result
+}
+
+/**
+ * Sending Fetch request
+ * @param {String} url - request url
+ * @returns {Promise} - Promise with request result
+ */
+
 export const getApiResource = async (url) => {
   try {
     const res = await fetch(url)
