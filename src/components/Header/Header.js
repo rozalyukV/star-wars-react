a141360e81eee3593da55e87201dd667b6da3b3b
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-05-03 19:42:45 +0200

    created PersonLinkBack component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,19 +7,13 @@ const Header = () => {
     <div className={styles.container}>
       <ul className={styles.list__container}>
         <li>
-          <NavLink to="/" end>
-            Home
-          </NavLink>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <NavLink to="/people/?page=1" end>
-            People
-          </NavLink>
+          <NavLink to="/people?page=1">People</NavLink>
         </li>
         <li>
-          <NavLink to="/not-found" end>
-            Not Found
-          </NavLink>
+          <NavLink to="/not-found">Not Found</NavLink>
         </li>
       </ul>
     </div>
--- src/containers/PersonPage/PersonPage.js
+++ src/containers/PersonPage/PersonPage.js
@@ -6,6 +6,7 @@ import { withErrorApi } from '@hoc-helpers/withErrorApi'
 
 import PersonInfo from '@components/PersonPage/PersonInfo'
 import PersonPhoto from '@components/PersonPage/PersonPhoto'
+import PersonLinkBack from '@components/PersonPage/PersonLinkBack'
 
 import { getApiResource } from '@utils/network'
 import { getPeopleImage } from '@services/getPeopleData'
@@ -48,6 +49,7 @@ const PersonPage = ({ setErrorApi }) => {
 
   return (
     <>
+      <PersonLinkBack />
       <div className={styles.wrapper}>
         <span className={styles.person__name}>{personName}</span>
         <div className={styles.container}>
