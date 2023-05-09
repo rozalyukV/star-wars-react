095c9c6271b4b8c1336fc76cf2ca933ecc745704
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-05-10 00:04:33 +0200

    refactored PersonPhoto component

--- src/components/PersonPage/PersonPhoto/PersonPhoto.js
+++ src/components/PersonPage/PersonPhoto/PersonPhoto.js
@@ -1,17 +1,41 @@
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
+
+import { setPersonToFavorite, removePersonFromFavorite } from '@store/actions'
+
 import styles from './PersonPhoto.module.css'
 
-const PersonPhoto = ({ personPhoto, personName }) => {
+const PersonPhoto = ({ personId, personPhoto, personName }) => {
+  const dispatch = useDispatch()
+
+  const set = () => {
+    dispatch(
+      setPersonToFavorite({
+        [personId]: {
+          name: personName,
+          img: personPhoto,
+        },
+      })
+    )
+  }
+
+  const remove = () => {
+    dispatch(removePersonFromFavorite(personId))
+  }
+
   return (
     <>
       <div className={styles.container}>
         <img className={styles.photo} src={personPhoto} alt={personName} />
       </div>
+      <button onClick={set}>Add to favorites</button>
+      <button onClick={remove}>Remove from favorites</button>
     </>
   )
 }
 
 PersonPhoto.propTypes = {
+  personId: PropTypes.string,
   personPhoto: PropTypes.string,
   personName: PropTypes.string,
 }
