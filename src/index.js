77a262ebda2f4e6f4b2a3e5071309f9964689793
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-05-09 16:46:52 +0200

    added Redux structure to the app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from '@store/store'
 
 import App from '@containers/App'
 
@@ -8,6 +10,8 @@ import '@styles/index.css'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
