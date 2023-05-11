13c0c065bc9d0d2e5abb9f91dd1b1522ab9b4066
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-05-11 18:44:33 +0200

    connected Context API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from '@store/store'
 
+import ThemeProvider from '@context/ThemeProvider'
 import App from '@containers/App'
 
 import '@styles/index.css'
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 )
