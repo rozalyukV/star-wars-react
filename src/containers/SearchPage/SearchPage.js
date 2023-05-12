455629837b0f965d75252e60b11ace62e536b0c2
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-05-12 12:53:09 +0200

    refactored SearchPage container

--- src/containers/SearchPage/SearchPage.js
+++ src/containers/SearchPage/SearchPage.js
@@ -1,15 +1,36 @@
 import { useState } from 'react'
 import PropTypes from 'prop-types'
 
+import { withErrorApi } from '@hoc-helpers/withErrorApi'
+import { API_SEARCH } from '@constants/api'
 import { getApiResource } from '@utils/network'
+import { getPeopleId, getPeopleImage } from '@services/getPeopleData'
 
 import styles from './SearchPage.module.css'
 
-const SearchPage = () => {
+const SearchPage = ({ setErrorApi }) => {
   const [inputSearchValue, setInputSearchValue] = useState('')
+  const [people, setPeople] = useState([])
 
   const getResponse = async (param) => {
-    const res = await getApiResource()
+    const res = await getApiResource(API_SEARCH + param)
+
+    if (res) {
+      const peopleList = res.results.map(({ name, url }) => {
+        const id = getPeopleId(url)
+        const img = getPeopleImage(id)
+
+        return {
+          id,
+          name,
+          img,
+        }
+      })
+      setPeople(peopleList)
+      setErrorApi(false)
+    } else {
+      setErrorApi(true)
+    }
   }
 
   const handleInputChange = (event) => {
@@ -33,7 +54,7 @@ const SearchPage = () => {
 }
 
 SearchPage.propTypes = {
-  //   propName: PropTypes.string,
+  setErrorApi: PropTypes.func,
 }
 
-export default SearchPage
+export default withErrorApi(SearchPage)
