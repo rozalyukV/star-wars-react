566ee73f19346e7889081402702b8563d7005b6f
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-05-12 10:48:17 +0200

    refactored ChooseSide component

--- src/components/HomePage/ChooseSide/ChooseSide.js
+++ src/components/HomePage/ChooseSide/ChooseSide.js
@@ -1,4 +1,6 @@
-import PropTypes from 'prop-types'
+import PropTypes, { element } from 'prop-types'
+import cn from 'classnames'
+
 import styles from './ChooseSide.module.css'
 
 import {
@@ -8,21 +10,68 @@ import {
   THEME_NEUTRAL,
 } from '@context/ThemeProvider'
 
-const ChooseSide = () => {
+import imgLightSide from './img/light-side.jpg'
+import imgDarkSide from './img/dark-side.jpg'
+import imgFalcon from './img/falcon.jpg'
+
+const ChooseSideItem = ({ theme, text, img, classes }) => {
   const isTheme = useTheme()
 
   return (
-    <>
-      {isTheme.theme}
-      <button onClick={() => isTheme.change(THEME_LIGHT)}>Light</button>
-      <button onClick={() => isTheme.change(THEME_DARK)}>Dark</button>
-      <button onClick={() => isTheme.change(THEME_NEUTRAL)}>Neutral</button>
-    </>
+    <div
+      className={cn(styles.item, classes)}
+      onClick={() => {
+        isTheme.change(theme)
+      }}
+    >
+      <div className={styles.item__header}>{text}</div>
+      <img className={styles.item__img} src={img} alt={text} />
+    </div>
   )
 }
 
-ChooseSide.propTypes = {
-  //   propName: PropTypes.string,
+ChooseSideItem.propTypes = {
+  theme: PropTypes.string,
+  text: PropTypes.string,
+  img: PropTypes.string,
+  classes: PropTypes.string,
+}
+
+const ChooseSide = () => {
+  const elements = [
+    {
+      theme: THEME_LIGHT,
+      text: 'Light Side',
+      img: imgLightSide,
+      classes: styles.item__light,
+    },
+    {
+      theme: THEME_DARK,
+      text: 'Dark Side',
+      img: imgDarkSide,
+      classes: styles.item__dark,
+    },
+    {
+      theme: THEME_NEUTRAL,
+      text: 'Neutral Side',
+      img: imgFalcon,
+      classes: styles.item__neutral,
+    },
+  ]
+
+  return (
+    <div className={styles.container}>
+      {elements.map(({ theme, text, img, classes }, index) => (
+        <ChooseSideItem
+          key={index}
+          theme={theme}
+          text={text}
+          img={img}
+          classes={classes}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default ChooseSide
